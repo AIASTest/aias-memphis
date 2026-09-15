b9d54129d1b70f9a818c535a701aead49a23244f
AIASTest <aiaistest@gmail.com>
2026-09-15 03:20:42 -0500

    Load Design Studio game layer after core engine

--- assets/js/designer.js
+++ assets/js/designer.js
@@ -1,9 +1,25 @@
 (() => {
   'use strict';
   const current = document.currentScript;
-  const script = document.createElement('script');
-  script.src = 'assets/js/designer-v2.js';
-  script.defer = true;
-  if (current?.parentNode) current.parentNode.insertBefore(script, current.nextSibling);
-  else document.head.appendChild(script);
+
+  const style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.href = 'assets/css/designer-fun.css';
+  document.head.appendChild(style);
+
+  const core = document.createElement('script');
+  core.src = 'assets/js/designer-v2.js';
+  core.defer = true;
+
+  const fun = document.createElement('script');
+  fun.src = 'assets/js/designer-fun.js';
+  fun.defer = true;
+
+  core.addEventListener('load', () => {
+    if (core.parentNode) core.parentNode.insertBefore(fun, core.nextSibling);
+    else document.head.appendChild(fun);
+  });
+
+  if (current?.parentNode) current.parentNode.insertBefore(core, current.nextSibling);
+  else document.head.appendChild(core);
 })();
